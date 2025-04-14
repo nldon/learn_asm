ace86a0556c073f3011753ed2c9b79d6514ef1d0
nld91 <nathanleedonovan@gmail.com>
2025-04-14 23:52:13 +0100

    Added basic error handling, some optimisation

--- read_write/read_write.s
+++ read_write/read_write.s
@@ -1,9 +1,10 @@
 .section .data
+    empty_err: .asciz "Error: Input can not be empty\n"
+    empty_err_len = . - empty_err
 
 .section .bss
     in_buf: .skip 32
     in_buf_len = . - in_buf
-    read_bytes: .skip 8
 
 .section .text
 .globl _start
@@ -16,19 +17,35 @@ _start:
     lea in_buf(%rip), %rsi
     mov $in_buf_len, %rdx
     syscall
+    # test for empty input (null), not possible from keyboard as enter
+    # submits a newline at least. Doable with cat /dev/null
+    test %rax, %rax
+    jz handle_empty
+    # test for just the newline char, submit with no characters written
+    cmpb $10, in_buf(%rip)
+    je handle_empty
 
-    # returns num of bytes read
-    mov %rax, read_bytes(%rip)
-
-    # write
+    # write(fd, *buf, count) syscall 1
+    # rdi, rsi, rdx
+    # can push the value in rax straight into rdx here rather than use an
+    # intermediate label to save an instruction.
+    # also don't have to set rsi again, it's still pointing to the buffer
+    # mem address
+    mov %rax, %rdx
     mov $1, %rax
     mov $1, %rdi
-    # rsi shouldn't have changed?
-    mov read_bytes(%rip), %rdx
     syscall
+    jmp exit_program
 
-    # exit
+exit_program:
     mov $60, %rax
     mov $0, %rdi
     syscall
 
+handle_empty:
+    mov $1, %rax
+    mov $1, %rdi
+    lea empty_err(%rip), %rsi
+    mov $empty_err_len, %rdx
+    syscall
+    jmp exit_program
